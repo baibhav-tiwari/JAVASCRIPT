6b27a4606657d8bb04a88ab00bc8c0b63591a941
baibhav tiwari <baibhavtiwari37@gmail.com>
2022-10-11 09:45:45 +0530

    commit

--- arithopertaors.js
+++ arithopertaors.js
@@ -1,4 +1,8 @@
-let x;
-x = 8;
-console.log(x);
-
+let x, y;
+x = 9;
+y = 6;
+z = x + y;// here you can apply other arithmetic operator 
+z1 = x - y;
+z2 = x * y;
+z3 = x / y;
+console.log(x, y, z, z1, z2, z3);
