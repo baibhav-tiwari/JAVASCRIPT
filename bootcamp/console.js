55fc2cd142fba74d59b19e2aff11e553fc4d9fc7
baibhav <baibhavtiwari37@gmail.com>
2022-11-13 11:03:48 +0530

    console

---
+++ bootcamp/console.js
@@ -0,0 +1,7 @@
+console.log("hello world")
+console.log("baibhav tiwari")
+name = "baibhav"
+console.log(name)
+intro = " hey everyone "
+sentence = ", this side this is my first program in javascript,which i write while learning the language"
+console.log(intro + name + sentence)
